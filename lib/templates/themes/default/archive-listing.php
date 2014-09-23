f4ea5672fcc1f6f4f16c3a30f93e907ab8c6ef97
mervb1 <mervb1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-23 06:32:42 +0000

    = 1.2.1: September 23, 2014 = * Fix: Search Widget not working on page 2 of archive page in some instances * Fix: Property feature list Toilet and New Construction now display in list when ticked * Fix: EPL - Listing widget was not displaying featured listings * Fix: Allowed to filter by commercial_listing_type in [listing_category] shortcode * Fix: Updated templates to display Search Results when performing search * New: Added initial Dutch translation.
    git-svn-id: https://plugins.svn.wordpress.org/easy-property-listings/tags/1.2.1@995195 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/templates/themes/default/archive-listing.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Archive Template for Property Custom Post Type : property
+ */
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+ 
+get_header(); ?>
+<section id="primary" class="site-content">
+	<div id="content" role="main">
+		<?php
+		if ( have_posts() ) : ?>
+			<div class="loop">
+				<header class="archive-header entry-header loop-header">
+					<h4 class="archive-title loop-title">
+						<?php
+							the_post();
+							 
+							if ( is_tax() && function_exists( 'epl_is_search' ) && false == epl_is_search() ) { // Tag Archive
+								$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+								$title = sprintf( __( 'Property in %s', 'epl' ), $term->name );
+							}
+							else if ( function_exists( 'epl_is_search' ) && epl_is_search() ) { // Search Result
+								$title = __( 'Search Result', 'epl' );
+							}
+							
+							else if ( function_exists( 'is_post_type_archive' ) && is_post_type_archive() && function_exists( 'post_type_archive_title' ) ) { // Post Type Archive
+								$title = post_type_archive_title( '', false );
+							} 
+							
+							else { // Default catchall just in case
+								$title = __( 'Listing', 'epl' );
+							}
+							
+							if ( is_paged() )
+								printf( '%s &ndash; Page %d', $title, get_query_var( 'paged' ) );
+							else
+								echo $title;
+							
+							rewind_posts();
+						?>
+					</h4>
+				</header>
+				
+				<div class="entry-content loop-content">
+					<?php
+						while ( have_posts() ) : // The Loop
+							the_post();
+							echo epl_property_blog();
+						endwhile; // end of one post
+					?>
+				</div>
+				
+				<div class="loop-footer">
+					<!-- Previous/Next page navigation -->
+					<div class="loop-utility clearfix">
+						<div class="alignleft"><?php previous_posts_link( __( '&laquo; Previous Page', 'epl' ) ); ?></div>
+						<div class="alignright"><?php next_posts_link( __( 'Next Page &raquo;', 'epl' ) ); ?></div>
+					</div>
+				</div>
+			</div>
+		<?php endif; ?>
+	</div>
+</section>
+<?php
+get_sidebar();
+get_footer(); 
---
+++ lib/widgets/widget-listing-search.php
@@ -0,0 +1,321 @@
+<?php
+/**
+ * WIDGET :: Property Search
+ *
+ * @package     EPL
+ * @subpackage  Widget/Search
+ * @copyright   Copyright (c) 2014, Merv Barrett
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.0
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class EPL_Widget_Property_Search extends WP_Widget {
+
+	function __construct() {
+		parent::WP_Widget( false, $name = __('EPL - Listing Search', 'epl') );
+	}
+
+	function widget($args, $instance) {	
+		$defaults = array(
+			'title'				=>	'',
+			'post_type'			=>	'property',
+			'property_status'	=>	'any',
+			'search_house_category'	=>	'on',
+			'search_price'		=>	'on',
+			'search_bed'		=>	'on',
+			'search_bath'		=>	'on',
+			'search_car'		=>	'on',
+			'search_other'		=>	'on'
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults ); 
+		extract( $args );
+		
+		echo $before_widget;
+		
+		$title	= apply_filters('widget_title', $instance['title']);
+		if ( $title )
+			echo $before_title . $title . $after_title;			
+		
+		$instance['show_title'] = false;
+		echo epl_shortcode_listing_search_callback($instance);
+		
+		echo $after_widget;
+	}
+
+	function update($new_instance, $old_instance) {		
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['post_type'] = strip_tags($new_instance['post_type']);
+		$instance['property_status'] = strip_tags($new_instance['property_status']);
+		$instance['search_house_category'] = strip_tags($new_instance['search_house_category']);
+		$instance['search_price'] = strip_tags($new_instance['search_price']);
+		$instance['search_bed'] = strip_tags($new_instance['search_bed']);
+		$instance['search_bath'] = strip_tags($new_instance['search_bath']);
+		$instance['search_car'] = strip_tags($new_instance['search_car']);
+		$instance['search_other'] = strip_tags($new_instance['search_other']);
+		return $instance;
+	}
+
+	function form($instance) {
+		$defaults = array(
+			'title'				=>	'',
+			'post_type'			=>	'property',
+			'property_status'	=>	'any',
+			'search_house_category'	=>	'on',
+			'search_price'		=>	'on',
+			'search_bed'		=>	'on',
+			'search_bath'		=>	'on',
+			'search_car'		=>	'on',
+			'search_other'		=>	'on'
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults ); 	
+	
+		$title				=	esc_attr($instance['title']);
+		$post_type			=	esc_attr($instance['post_type']);
+		$property_status	=	esc_attr($instance['property_status']);
+		$search_house_category	=	esc_attr($instance['search_house_category']);
+		$search_price		=	esc_attr($instance['search_price']);
+		$search_bed			=	esc_attr($instance['search_bed']);
+		$search_bath		=	esc_attr($instance['search_bath']);
+		$search_car			=	esc_attr($instance['search_car']);
+		$search_other		=	esc_attr($instance['search_other']);
+		?>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'epl'); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id('post_type'); ?>"><?php _e('Property Type:', 'epl'); ?></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id('post_type'); ?>" name="<?php echo $this->get_field_name('post_type'); ?>">
+				<?php
+					$post_types = epl_get_active_post_types();
+					if(!empty($post_types)) {
+						foreach($post_types as $k=>$v) {
+							$selected = '';
+							if($k == $post_type) {
+								$selected = 'selected="selected"';
+							}
+							echo '<option value="'.$k.'" '.$selected.'>'.__($v, 'epl').'</option>';
+						}
+					}
+				?>
+			</select>
+		</p>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id('property_status'); ?>"><?php _e('Status:', 'epl'); ?></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id('property_status'); ?>" name="<?php echo $this->get_field_name('property_status'); ?>">
+				<?php
+					$status_list = array(
+						''			=>	'Any',
+						'current'	=>	'Current',
+						'sold'		=>	'Sold',
+						'leased'	=>	'Leased'
+					);
+					
+					foreach($status_list as $k=>$v) {
+						$selected = '';
+						if(isset($property_status) && $k == $property_status) {
+							$selected = 'selected="selected"';
+						}
+						echo '<option value="'.$k.'" '.$selected.'>'.__($v, 'epl').'</option>';
+					}
+				?>
+			</select>
+		</p>
+
+		<p>
+			<input id="<?php echo $this->get_field_id('search_house_category'); ?>" name="<?php echo $this->get_field_name('search_house_category'); ?>" type="checkbox" <?php if(isset($search_house_category) && $search_house_category == 'on') { echo 'checked="checked"'; } ?> />
+			<label for="<?php echo $this->get_field_id('search_house_category'); ?>"><?php _e('House Category', 'epl'); ?></label>
+		</p>
+		<p>
+			<input id="<?php echo $this->get_field_id('search_price'); ?>" name="<?php echo $this->get_field_name('search_price'); ?>" type="checkbox" <?php if(isset($search_price) && $search_price == 'on') { echo 'checked="checked"'; } ?> />
+			<label for="<?php echo $this->get_field_id('search_price'); ?>"><?php _e('Price', 'epl'); ?></label>
+		</p>
+		<p>
+			<input id="<?php echo $this->get_field_id('search_bed'); ?>" name="<?php echo $this->get_field_name('search_bed'); ?>" type="checkbox" <?php if(isset($search_bed) && $search_bed == 'on') { echo 'checked="checked"'; } ?> />
+			<label for="<?php echo $this->get_field_id('search_bed'); ?>"><?php _e('Bedroom', 'epl'); ?></label>
+		</p>
+		<p>
+			<input id="<?php echo $this->get_field_id('search_bath'); ?>" name="<?php echo $this->get_field_name('search_bath'); ?>" type="checkbox" <?php if(isset($search_bath) && $search_bath == 'on') { echo 'checked="checked"'; } ?> />
+			<label for="<?php echo $this->get_field_id('search_bath'); ?>"><?php _e('Bathroom', 'epl'); ?></label>
+		</p>
+		<p>
+			<input id="<?php echo $this->get_field_id('search_car'); ?>" name="<?php echo $this->get_field_name('search_car'); ?>" type="checkbox" <?php if(isset($search_car) && $search_car == 'on') { echo 'checked="checked"'; } ?> />
+			<label for="<?php echo $this->get_field_id('search_car'); ?>"><?php _e('Car', 'epl'); ?></label>
+		</p>
+		<p>
+			<input id="<?php echo $this->get_field_id('search_other'); ?>" name="<?php echo $this->get_field_name('search_other'); ?>" type="checkbox" <?php if(isset($search_other) && $search_other == 'on') { echo 'checked="checked"'; } ?> />
+			<label for="<?php echo $this->get_field_id('search_other'); ?>"><?php _e('Other Search Options', 'epl'); ?></label>
+		</p>
+		
+		<?php 
+	}
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("EPL_Widget_Property_Search");') );
+
+//Property Search Query
+function epl_search_pre_get_posts( $query ) {
+	if ( is_admin() || !$query->is_main_query() ) {
+		return;
+	}
+	
+	if( epl_is_search() ) {
+		$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+		
+		$query->init();
+		$query->set('posts_per_page', get_option('posts_per_page'));
+		$query->set('paged', $paged);
+		
+		extract($_REQUEST);
+		if(isset($post_type) && !empty($post_type)) {
+			$query->set('post_type', $post_type);
+		} else {
+			$epl_post_types = epl_get_active_post_types();
+			if(!empty($epl_post_types)) {
+				$epl_post_types = array_keys($epl_post_types);
+				$query->set('post_type', $epl_post_types);
+			}
+		}
+
+		$meta_query = array();
+		if(isset($property_security_system) && !empty($property_security_system)) {
+			$meta_query[] = array(
+				'key'		=>	'property_security_system',
+				'value'		=>	array('yes', '1'),
+				'compare'	=>	'IN'
+			);
+		}
+		if(isset($property_air_conditioning) && !empty($property_air_conditioning)) {
+			$meta_query[] = array(
+				'key'		=>	'property_air_conditioning',
+				'value'		=>	array('yes', '1'),
+				'compare'	=>	'IN'
+			);
+		}
+		if(isset($property_pool) && !empty($property_pool)) {
+			$meta_query[] = array(
+				'key'		=>	'property_pool',
+				'value'		=>	array('yes', '1'),
+				'compare'	=>	'IN'
+			);
+		}
+		if(isset($property_bedrooms_min) && !empty($property_bedrooms_min)) {
+			$meta_query[] = array(
+				'key'		=>	'property_bedrooms',
+				'value'		=>	$property_bedrooms_min,
+				'type'		=>	'numeric',
+				'compare'	=>	'>='
+			);
+		}
+		if(isset($property_bedrooms_max) && !empty($property_bedrooms_max)) {
+			$meta_query[] = array(
+				'key'		=>	'property_bedrooms',
+				'value'		=>	$property_bedrooms_max,
+				'type'		=>	'numeric',
+				'compare'	=>	'<='
+			);
+		}
+		if(isset($property_bathrooms) && !empty($property_bathrooms)) {
+			$meta_query[] = array(
+				'key'		=>	'property_bathrooms',
+				'value'		=>	$property_bathrooms,
+				'type'		=>	'numeric',
+				'compare'	=>	'>='
+			);
+		}
+		if(isset($property_carport) && !empty($property_carport)) {
+			$meta_query[] = array(
+				'key'		=>	'property_carport',
+				'value'		=>	$property_carport,
+				'type'		=>	'numeric',
+				'compare'	=>	'>='
+			);
+		}
+		if(isset($property_category) && !empty($property_category)) {
+			$meta_query[] = array(
+				'key'		=>	'property_category',
+				'value'		=>	$property_category,
+				'compare'	=>	'='
+			);
+		}
+		
+		if( isset($post_type) && ($post_type == 'rental' || $post_type == 'commercial' || $post_type == 'business' || $post_type == 'holiday_rental' || $post_type == 'commercial_land') ) {
+			$key = 'property_rent';
+		} else {
+			$key = 'property_price';
+		}			
+		if(isset($property_price_from) && !empty($property_price_from)) {
+			$meta_query[] = array(
+				'key'		=>	$key,
+				'value'		=>	$property_price_from,
+				'type'		=>	'numeric',
+				'compare'	=>	'>='
+			);
+		}
+		if(isset($property_price_to) && !empty($property_price_to)) {
+			$meta_query[] = array(
+				'key'		=>	$key,
+				'value'		=>	$property_price_to,
+				'type'		=>	'numeric',
+				'compare'	=>	'<='
+			);
+		}
+		
+		if(isset($property_status) && !empty($property_status)) {
+			$meta_query[] = array(
+				'key'		=>	'property_status',
+				'value'		=>	$property_status,
+				'compare'	=>	'='
+			);
+		}
+		
+		if(!empty($meta_query)) {
+			$query->set('meta_query', $meta_query);;
+		}
+		
+		$tax_query = array();
+		if(isset($property_location) && !empty($property_location)) {
+			$tax_query[] = array(
+				'taxonomy'	=>	'location',
+				'field'		=>	'id',
+				'terms'		=>	$property_location
+			);
+		}
+		
+		if(!empty($tax_query)) {
+			$query->set('tax_query', $tax_query);;
+		}
+		$query->parse_query();
+	}
+}
+add_action( 'pre_get_posts', 'epl_search_pre_get_posts' );
+
+//Is Property Search
+function epl_is_search() {
+	if(isset($_REQUEST['action']) && $_REQUEST['action'] == 'epl_search') {
+		return true;
+	}
+	return false;
+}
+
+function epl_get_meta_values( $key = '', $type = 'post', $status = 'publish' ) {
+	if( empty($key) ) {
+		return;
+	}
+	
+	global $wpdb;	
+	$results = $wpdb->get_results( $wpdb->prepare( "SELECT distinct(pm.`meta_value`) FROM {$wpdb->postmeta} pm LEFT JOIN {$wpdb->posts} p ON p.`ID` = pm.`post_id` WHERE pm.`meta_key` = '%s' AND p.`post_status` = '%s' AND p.`post_type` = '%s' AND pm.`meta_value` != ''", $key, $status, $type ));
+	if(!empty($results)) {
+		$return = array();
+		foreach($results as $result) {
+			$return[] = $result->meta_value;
+		}
+		return $return;
+	}
+}
